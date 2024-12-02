406673f62a5f6e3541515d0695711e5a8edee510
Tiago Farinas <tiagofarinas@gmail.com>
2024-12-02 17:58:46 -0500

    Final Changes

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ new Chart(
     ctx,{
         type:'bar',
         data: {
-            labels:['Red','Blue','Yellow'],//X axis labels
+            labels:['Red','Blue','Yellow','Black','Green'],//X axis labels
             datasets:[
                 {
                     label: "Votes",
-                    data:[12,19,3],
-                    backgroundColor: ['red','blue','yellow']
+                    data:[12,19,3,20,7],
+                    borderWidth: 2,
+                    backgroundColor: ['rgba(255, 13, 62, 0.5)',
+                                      'rgba(13, 168, 255, 0.5)',
+                                      'rgba(210, 251, 12, 0.5)',
+                                      'rgba(0, 0, 0, 0.5)',
+                                      'rgba(16, 197, 40, 0.5)'],
+                    borderColor: ['rgba(255, 13, 62, 1)','rgba(13, 168, 255, 1)','rgba(210, 251, 12, 1)','rgba(0,0,0,1)','rgba(16, 197, 40, 1)'] ,
+                    barThickness: 60,
                 }
             ]
         }
